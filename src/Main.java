c21a9a4ae1e906fdccda080f0b3c8cf5a51258c3
Yevhen Lepet <sadjem1990@gmail.com>
2019-10-08 08:56:29 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        int [] [] grid = {{0,1,0,0},{1,1,1,0},{0,1,0,0},{1,1,0,0}};
+        int result = islandPerimeter(grid);
+        System.out.println(result);
+    }
+    public static int islandPerimeter(int[][] grid){
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int result = 0;
+        for (int i = 0; i < grid.length ; i++) {
+            for (int j = 0; j < grid[i].length ; j++) {
+                if(grid[i][j] == 1){
+                    result +=4;
+                    if(i > 0 && grid[i-1][j] == 1){
+                        result -=2;
+                    }
+                    if (j > 0 && grid[i][j-1] == 1){
+                        result -=2;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
